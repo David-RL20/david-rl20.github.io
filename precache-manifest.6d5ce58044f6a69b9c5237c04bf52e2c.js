dd820acfc803a803061db3dcb41a555156b9e573
David Rodriguez Leal <60858689+David-RL20@users.noreply.github.com>
2020-08-18 10:44:50 -0700

    Upload files IoT project
    Temporary files of IoT project

---
+++ precache-manifest.6d5ce58044f6a69b9c5237c04bf52e2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9769be437ce40f31d79882005b1e54b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e220fe94fc4629c40f",
+    "url": "/static/css/main.c8567a91.chunk.css"
+  },
+  {
+    "revision": "e8eb1c1c78a7193f81ff",
+    "url": "/static/js/2.82b867ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.82b867ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e220fe94fc4629c40f",
+    "url": "/static/js/main.a1e1051b.chunk.js"
+  },
+  {
+    "revision": "f3631654c70f5b6801eb",
+    "url": "/static/js/runtime-main.1eabd3b0.js"
+  },
+  {
+    "revision": "9cf0a1adee34b11d10d8c8d2e3e53438",
+    "url": "/static/media/not-found-404.9cf0a1ad.png"
+  },
+  {
+    "revision": "559bf25eb57b6d4a551352e93af22659",
+    "url": "/static/media/ui_face.559bf25e.jpg"
+  }
+]);
